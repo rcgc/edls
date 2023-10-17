026de8180fdc1e43f76600a3aed9f5d174a12fd7
rcgc <roberto140298@gmail.com>
2023-10-17 17:53:18 -0600

    first part of project completed

---
+++ edls.go
@@ -0,0 +1,24 @@
+package main
+
+// file types
+const (
+	fileRegula int = iota
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileLink
+)
+
+// file extension
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	gz = ".gz"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpg = ".jpg"
+	gif = ".gif"
+)
